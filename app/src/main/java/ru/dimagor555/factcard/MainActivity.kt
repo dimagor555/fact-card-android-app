3cbe84f27b2c45d0dd8c1370c3888b3cedfec9f1
dimagor555 <dimagor555@mail.ru>
2021-04-19 22:29:06 +0300

    Initial commit

---
+++ app/src/main/java/ru/dimagor555/factcard/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.dimagor555.factcard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
