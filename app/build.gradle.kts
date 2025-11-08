72b05c46400d126362e31f53ed7b88cee8110ce7
dimagor555 <dimagor555@mail.ru>
2025-11-08 19:42:10 +0700

    Bump version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.dimagor555.factcard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
